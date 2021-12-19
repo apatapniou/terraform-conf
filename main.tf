0f01e3105261a7ceb33efe42e3ef75014c4d8985
aliaksandr.patapniou <aliaksandr.patapniou@syncplicity.com>
2021-12-19 21:22:44 +0300

    Refactor code to module

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.68.0"
-    }
-  }
-  backend "s3" {
-    bucket = "apatapniou-terraform-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
