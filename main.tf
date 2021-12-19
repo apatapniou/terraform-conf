e08524e01ac9d4079e9b4cf2ebc35889661a5a1f
aliaksandr.patapniou <aliaksandr.patapniou@syncplicity.com>
2021-12-19 22:02:57 +0300

    Refactor code to module

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {}
   required_version = ">= 0.15"
-}
-
-provider "aws" {
-  region  = var.aws_region
-  version = ">= 3.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
 }
